741808f57e010607b4e0f95461bd39d3be4b2a4a
Keven <kevendicamargo@gmail.com>
2025-12-10 17:08:52 -0300

    chore: adicionar TypeScript ao projeto e configurar ambiente

---
+++ src/custom.d.ts
@@ -0,0 +1,14 @@
+declare module "*.sass" {
+  const content: Record<string, string>;
+  export default content;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_EMAILJS_SERVICE_ID: string;
+  readonly VITE_EMAILJS_TEMPLATE_ID: string;
+  readonly VITE_EMAILJS_PUBLIC_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
